d48bf36908e0da80462dff490c91ef66ee0dabc2
luffy1610 <luffy1610@gmail.com>
2021-02-18 01:37:56 +0800

    feat: Complete ksql.py in consumer
    Aggregate counting from raw_turnstile topic

--- Optimizing Public Transportation/consumer/ksql.py
+++ Optimizing Public Transportation/consumer/ksql.py
@@ -12,25 +12,23 @@ logger = logging.getLogger(__name__)
 
 KSQL_URL = "http://localhost:8088"
 
-#
-# TODO: Complete the following KSQL statements.
-# TODO: For the first statement, create a `turnstile` table from your turnstile topic.
-#       Make sure to use 'avro' datatype!
-# TODO: For the second statment, create a `turnstile_summary` table by selecting from the
-#       `turnstile` table and grouping on station_id.
-#       Make sure to cast the COUNT of station id to `count`
-#       Make sure to set the value format to JSON
 
 KSQL_STATEMENT = """
 CREATE TABLE turnstile (
-    ???
+    station_id INT,
+    station_name VARCHAR,
+    line VARCHAR
 ) WITH (
-    ???
+    KAFKA_TOPIC='raw_turnstile',
+    VALUE_FORMAT='AVRO',
+    KEY='station_id'
 );
 
 CREATE TABLE turnstile_summary
-WITH (???) AS
-    ???
+WITH (VALUE_FORMAT='JSON') AS
+    SELECT station_id, COUNT(*) AS count
+    FROM turnstile
+    GROUP BY station_id;
 """
 
 
